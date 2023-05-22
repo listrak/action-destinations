6579cb7d46b6cfa44958548b384dddf2b5d66e32
Eric Hinerdeer <eric.hinerdeer@listrak.com>
2023-05-22 15:18:27 -0400

    fixing index

--- packages/destination-actions/src/destinations/listrak/index.ts
+++ packages/destination-actions/src/destinations/listrak/index.ts
@@ -13,6 +13,7 @@ const destination: DestinationDefinition<Settings> = {
     scheme: 'oauth2',
     fields: {},
     testAuthentication: (request) => {
+      console.log(request);
       // Return a request that tests/validates the user's credentials.
       // If you do not have a way to validate the authentication fields safely,
       // you can remove the `testAuthentication` function, though discouraged.
@@ -22,16 +23,16 @@ const destination: DestinationDefinition<Settings> = {
 
       // TODO: Write an actual auth. This is commented out for local testing.
 
-      // const res = await request('https://www.example.com/oauth/refresh', {
-      //   method: 'POST',
-      //   body: new URLSearchParams({
-      //     refresh_token: auth.refreshToken,
-      //     client_id: auth.clientId,
-      //     client_secret: auth.clientSecret,
-      //     grant_type: 'refresh_token'
-      //   })
-      // })
-
+      const res = await request('https://www.example.com/oauth/refresh', {
+        method: 'POST',
+        body: new URLSearchParams({
+          refresh_token: auth.refreshToken,
+          client_id: auth.clientId,
+          client_secret: auth.clientSecret,
+          grant_type: 'refresh_token'
+        })
+      })
+      console.log(res);
       // return { accessToken: res.data.access_token }
       return { accessToken: 'token-we-want-to-use-to-auth' }
     }
@@ -45,6 +46,9 @@ const destination: DestinationDefinition<Settings> = {
   },
 
   onDelete: async (request, { settings, payload }) => {
+    console.log(request);
+    console.log(settings);
+    console.log(payload);
     // Return a request that performs a GDPR delete for the provided Segment userId or anonymousId
     // provided in the payload. If your destination does not support GDPR deletion you should not
     // implement this function and should remove it completely.
